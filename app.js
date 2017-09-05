c810df32d1f8719af3fda56cc85c67803f5214b5
dtthor <d.t.thorwaldson@gmail.com>
2017-09-05 14:58:57 -0500

    Score functionality works better

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ let score;
 let alive = true;
 
 // hero variables
-let heroWidth = 20;
-let heroHeight = 15;
+let heroWidth = 10;
+let heroHeight = 25;
 let heroSpeed = 40;
 let hero = {
     x: 0,
@@ -25,7 +25,7 @@ let hero = {
 // icicle variables
 let icicleWidth = 15;
 let icicleHeight = 15;
-let totalIcicles = 50;
+let totalIcicles = 60;
 let icicles = [];
 for (let i = 0; i < totalIcicles; i++) {
     addIcicle();
@@ -74,23 +74,22 @@ function animate() {
     // (1) check for collisions
     // (2) advance the icicle
     // (3) if the icicle falls below the canvas, reset that icicle
+    // (4) increment score if still alive
+
+    if(alive === true){
+        score++;
+    }
 
     for (let i = 0; i < icicles.length; i++) {
+      let icicle = icicles[i];
 
-        let icicle = icicles[i];
 
-        function incScore(){
-          score+=10;
-        }
-
-        while(alive === true){
-          setInterval()
-        }
 
         // test for icicle-hero collision
         if (isColliding(icicle, hero)) {
           alive = false;
-          alert('DEAD! Game over man! Game over.');
+          //ADD DEATH PAGE FUNCTION HERE
+          alert(`DEAD! Game over man! Game over. \n \n Score: ${score}`);
           window.location.reload(true);
 
         }
@@ -122,63 +121,32 @@ function drawEverything() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     // draw the background
-    context.fillStyle = "ivory";
+    context.fillStyle = "lightgray";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     // draw the hero
-    context.fillStyle = "skyblue";
+    context.fillStyle = "black";
     context.fillRect(hero.x, hero.y, hero.width, hero.height);
+    context.clearRect(hero.x+2, hero.y+2, 2, 2);
+    context.clearRect(hero.x+6, hero.y+2, 2, 2);
+    context.clearRect(hero.x+2, hero.y+6, 6, 1);
     context.strokeStyle = "lightgray";
     context.strokeRect(hero.x, hero.y, hero.width, hero.height);
-    // //DRAW OUR HERO'S HEAD
-    // let headX = 590;
-    // let headY = 565;
-    // let headW = 20;
-    // let headH = 20;
-    //
-    // function drawHead(headX, headY, headW, headH){
-    //   context.fillRect(headX, headY, headW, headH);
-    //   context.clearRect(headX+4, headY+5, 4, 4);
-    //   context.clearRect(headX+12, headY+5, 4, 4);
-    //   context.clearRect(headX+7, headY+14, 6, 2);
-    // };
-    //
-    // drawHead(headX, headY, headW, headH);
-    //
-    //
-    // //DRAW OUR HERO'S BODY
-    // let bodyX1 = 600;
-    // let bodyX2 = 595;
-    // let bodyX3 = 605;
-    //
-    // function drawBody(bodyX1, bodyX2, bodyX3) {
-    //   context.beginPath();
-    //   context.moveTo(bodyX1, 585);
-    //   context.lineTo(bodyX1, 590);
-    //   context.lineTo(bodyX2, 590);
-    //   context.lineTo(bodyX3, 590);
-    //   context.lineTo(bodyX1, 590);
-    //   context.lineTo(bodyX1, 595);
-    //   context.lineTo(bodyX2, 600);
-    //   context.lineTo(bodyX1, 595);
-    //   context.lineTo(bodyX3, 600);
-    //   context.stroke();
-    // };
-    //
-    // drawBody(bodyX1, bodyX2, bodyX3);
 
     // draw all icicles
     for (let i = 0; i < icicles.length; i++) {
         let icicle = icicles[i];
-        // optionally, drawImage(iciclesImg,icicle.x,icicle.y)
-        context.fillStyle = "gray";
+        // MAYBE ADD IMAGE OF ICICLE LATER, drawImage(iciclesImg,icicle.x,icicle.y)
+        context.fillStyle = "white";
+        context.strokeStyle = "black";
+        context.strokeRect(icicle.x, icicle.y, icicle.width, icicle.height);
         context.fillRect(icicle.x, icicle.y, icicle.width, icicle.height);
     }
 
     // draw the score
-    context.font = "14px Times New Roman";
+    context.font = "24px serif";
     context.fillStyle = "black";
-    context.fillText("Score: " + score, 10, 15);
+    context.fillText("Score: " + score, 10, 35);
 }
 
 
@@ -195,57 +163,6 @@ $("#start").click(function () {
         animate();
     };
 });
-
-
-
-  // let canvas = document.getElementById('game');
-  // let context = canvas.getContext('2d');
-  // let keepAnimating = false;
-  // let icicles = [];
-  // let totalIcicles = 1200;
-  // for (let i = 0; i < totalIcicles; i+=30) {
-  //   drawIcicles(i, i+10, i+20, i+10, i+20, i+30)
-  // };
-  //
-  // //HOW TO DRAW ICICLES
-  // function drawIcicles(x1, x2, x3, x4, x5, x6){
-  //   context.beginPath();
-  //   context.moveTo(x1, 0);
-  //   context.lineTo(x2, 30);
-  //   context.lineTo(x3, 0);
-  //   context.lineTo(x4, 0);
-  //   context.lineTo(x5, 30);
-  //   context.lineTo(x6, 0);
-  //   context.fill();
-  // };
-  //
-  //
-  //
-  // //MOVE OUR HERO
-  // window.onkeydown = function(ev) {
-  //   let keyPressed = ev.keyCode;
-  //   if(keyPressed === 39 && headX <= 1170){
-  //     headX += 20;
-  //     bodyX1 += 20;
-  //     bodyX2 += 20;
-  //     bodyX3 += 20; //right arrow add 20 from current
-  //   }else if(keyPressed === 37 && headX > 0){
-  //     headX -= 20; //left arrow subtract 20 from current
-  //     bodyX1 -= 20;
-  //     bodyX2 -= 20;
-  //     bodyX3 -= 20;
-  //   }
-  //
-  //
-  //   //CLEAR EVERYTHING ON CANVAS EXCEPT ICICLES
-  //   context.clearRect(0, 500, 1200, 600);
-  //
-  //   //DRAW HERO AT NEW POSITION
-  //   drawHead(headX, headY, headW, headH);
-  //   drawBody(bodyX1, bodyX2, bodyX3);
-  // };
-
-
 
 
 };
